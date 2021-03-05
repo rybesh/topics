841d5c48271ca047ed06cfc1c6e9ff16903ff7ce
Ryan Shaw <ryanshaw@unc.edu>
2021-03-05 14:05:59 -0500

    make topics script more robust

--- topics.py
+++ topics.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import shutil
 import subprocess
 from urllib.parse import quote
 from utils import pdf_path_to_doc_name, doc_name_to_fragment_id, get_model_name
@@ -17,8 +18,16 @@ port = sys.argv[4]
 doc_name = pdf_path_to_doc_name(pdf_path)
 fragment = quote(doc_name_to_fragment_id(doc_name))
 
-for p, t in doc_topics[doc_name]:
-    subprocess.run([
-        'open',
-        f'http://{host}:{port}/topdocs/{model}/{t}.html#{fragment}'
-    ])
+if doc_name in doc_topics:
+
+    open_cmd = shutil.which('open')
+    if open_cmd is None:
+        open_cmd = shutil.which('xdg-open')
+
+    for p, t in doc_topics[doc_name]:
+        url = f'http://{host}:{port}/topdocs/{model}/{t}.html#{fragment}'
+        print(url)
+        if open_cmd is not None:
+            subprocess.run([open_cmd, url])
+else:
+    print(f'not among the top documents for any of the topics in {model}')
